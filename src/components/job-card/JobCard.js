96e205f5abe67689fdaed73c3b533cda306f680c
Mahmoud Hboubati <mhmoud.21830@gmail.com>
2019-10-06 17:50:13 +0400

    feat(*): improve the card design

--- src/components/job-card/JobCard.js
+++ src/components/job-card/JobCard.js
@@ -15,9 +15,22 @@ class JobCard extends Component {
                     <h5 className="card-title">
                         {job.position}</h5>
                     <h6 className="card-subtitle mb-2 text-muted">{job.postedOn}</h6>
-                    <h6 className="card-subtitle mb-2 text-muted">{job.nationality}</h6>
                     <p className="card-text">{job.description}</p>
-                    <button className="btn btn-primary">APPLY AND OFFER</button>
+                    <div className="row">
+                        <div className="col-sm-4">
+                            <b>Nationality</b>
+                            <div className="detail-item-content">{job.nationality}</div>
+                        </div>
+                        <div className="col-sm-4">
+                            <b>Salary</b>
+                            <div className="detail-item-content">{job.salary}</div>
+                        </div>
+                        <div className="col-sm-4">
+                            <b>Last Date</b>
+                            <div className="detail-item-content">{job.endsOn}</div>
+                        </div>
+                    </div>
+                    <button className="btn btn-primary btn-apply">APPLY AND OFFER</button>
                 </div>
             </div>
         )
